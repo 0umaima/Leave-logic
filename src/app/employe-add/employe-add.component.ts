cb25f1b0fcff381cfad0841dd7a067f8f1c32f30
Melodic-n <welcomtochillis@gmail.com>
2024-05-10 20:32:45 +0100

    validation employe-add

--- src/app/employe-add/employe-add.component.ts
+++ src/app/employe-add/employe-add.component.ts
@@ -1,39 +1,51 @@
-import { EMPLOYEES } from './../models/user.model';
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute,Router, RouterLink } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employe-add.component.html',
   styleUrls: ['./employe-add.component.css'],
-  standalone:true ,
-  imports : [RouterLink ]
+  standalone: true,
+  imports: [RouterLink, NgIf]
 })
-export class EmployeeComponent implements OnInit {
-  employeeForm : any;
-  data: any;
+export class EmployeeComponent  {
+  employeeForm!: FormGroup;
+  newEmployee: any;
   submitted = false;
   show: boolean = false;
-  constructor (private formBuilder : FormBuilder,private route : ActivatedRoute ,private router : Router)  {}
 
-
-  ngOnInit(): void {
-
-
-}
-
-
-onSubmit() {
-  this.submitted = true;
-//add alert pop up
-}
-
-  formReset()
-  {
-   this.submitted=false;
+  constructor(private formBuilder: FormBuilder) {
+    this.employeeForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      department: ['', Validators.required],
+      role: ['', Validators.required],
+      solde: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      motdepass: ['', Validators.required],
+    });
   }
 
+  onSubmit() {
+    this.employeeForm.markAllAsTouched();
+    this.submitted = true;
+    this.newEmployee = this.employeeForm.value;
+    if (this.employeeForm.valid && this.submitted) {
+      alert('Le formulaire a été soumis avec succès!');
+    }  }
 
+  hasError(controlName: string, errorName: string): boolean {
+    const control = this.employeeForm.get(controlName);
+    return control ? control.hasError(errorName) && control.touched : false;
+  }
 
+  formReset() {
+    this.submitted = false;
+    this.employeeForm.reset();
+  }
 }
+
+
+
