ca5cc48edbe0b362f20a57b8aa3a44efa1868bbe
oumaima <o.khajaa@sophatel.com>
2024-05-10 15:32:58 +0100

    handle logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  public authService = inject(AuthenticationService);
+  private router = inject(Router)
+
+  hadleLogout() {
+    this.authService.logout().subscribe({
+      next: (data) => {
+        this.router.navigateByUrl('/login');
+      },
+    });
+  }
 }
